1ae3874c7212134b4a7898e22888cf52e8ff852f
Hoang Quoc Trung <hoang.quoc.trung@sun-asterisk.com>
2019-06-28 18:27:39 +0700

    add recorder

--- app/src/main/java/com/example/myapplication/Activity3.java
+++ app/src/main/java/com/example/myapplication/Activity3.java
@@ -1,25 +1,38 @@
 package com.example.myapplication;
 
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class Activity3 extends AppCompatActivity {
+public class Activity3 extends AppCompatActivity implements RecognitionListener {
     private static final int SYSTEM_ALERT_WINDOW_PERMISSION = 2084;
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView mVoiceInputTv;
     private ImageButton mSpeakBtn;
+    private SpeechRecognizer speech = null;
+    private String LOG_TAG = "VoiceRecognitionActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +53,17 @@ public class Activity3 extends AppCompatActivity {
     }
 
     private void startVoiceInput() {
+        /*speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech.setRecognitionListener(this);*/
+
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        intent.putExtra("android.speech.extra.GET_AUDIO_FORMAT", "audio/AMR");
+        intent.putExtra("android.speech.extra.GET_AUDIO", true);
+        //speech.startListening(intent);
         startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         startService(new Intent(Activity3.this, FloatWindowService.class));
 
@@ -58,14 +78,29 @@ public class Activity3 extends AppCompatActivity {
                 if (resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     mVoiceInputTv.setText(result.get(0));
-//                    Uri audioUri = data.getData();
-//                    ContentResolver contentResolver = getContentResolver();
-//                    try {
-//                        InputStream filestream = contentResolver.openInputStream(audioUri);
-//                        // TODO: read audio file from inputstream
-//                    } catch (FileNotFoundException e) {
-//                        e.printStackTrace();
-//                    }
+                    Uri audioUri = data.getData();
+                    ContentResolver contentResolver = getContentResolver();
+                    try {
+                        InputStream filestream = contentResolver.openInputStream(audioUri);
+                        ContextWrapper cw = new ContextWrapper(this);
+                        File directory = cw.getDir("audio", Context.MODE_PRIVATE);
+                        String extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(audioUri));
+                        Log.d("Record", extension);
+                        File mypath = new File(directory, "ibm." + extension);
+                        FileOutputStream fos = new FileOutputStream(mypath);
+                        byte[] data2 = new byte[1024];
+                        int count = filestream.read(data2);
+                        while (count!=-1){
+                            fos.write(data2,0,count);
+                            count = filestream.read(data2);
+                        }
+                        fos.flush();
+                        fos.close();
+                        filestream.close();
+                        // TODO: read audio file from inputstream
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             }
@@ -77,5 +112,55 @@ public class Activity3 extends AppCompatActivity {
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:" + getPackageName()));
         startActivityForResult(intent, SYSTEM_ALERT_WINDOW_PERMISSION);
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle params) {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onError(int error) {
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.i(LOG_TAG, "onResults");
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+        Log.i(LOG_TAG, "onPartialResults");
+        /*ArrayList<String> matches = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String text = "";
+        for (String result : matches)
+            text += result + "\n";
+        Log.i(LOG_TAG, text);*/
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+
     }
 }
